6d026397da70d7c5f1b5c3b04ca9e04fcb39bc69
ngocnguyenn <ngocnguyenn.1903@gmail.com>
2023-05-22 09:41:24 +0700

    Extent Report V5

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -8,6 +8,7 @@ public class GlobalConstants {
 	
 	public static final String PROJECT_PATH = System.getProperty("user.dir");
 	public static final String OS_NAME = System.getProperty("os.name");
+	public static final String JAVA_VERSION = System.getProperty("java.version");
 	public static final String UPLOAD_FILE = PROJECT_PATH + File.separator + "uploadFiles" + File.separator;
 	public static final String REPORTING_SCREENSHOT = PROJECT_PATH + File.separator + "reportNGImages" + File.separator;
 	public static final String DOWNLOAD_FILE = PROJECT_PATH + File.separator + "downloadFiles";
